22cc76caad4a6463641c1d181d205b2c682abd74
Shahrizal Shaari <shahrizal@binary.com>
2017-06-14 05:36:34 +0000

    add delta

--- lib/Math/Business/LookBacks/Greeks/Delta.pm
+++ lib/Math/Business/LookBacks/Greeks/Delta.pm
@@ -1,4 +1,4 @@
-package Math::Business::LookBacks::::Greeks::Delta;
+package Math::Business::LookBacks::Greeks::Delta;
 use strict;
 use warnings;
 
@@ -23,6 +23,39 @@ sub lbfloatput {
 
 sub lbhighlow {
     return 0.0;
+}
+
+sub _l_min_delta {
+    my ($S, $K, $t, $r_q, $mu, $sigma, $S_min) = @_;
+
+    my $a1 = (log($S / $S_min) + ($mu + 0.5 * ($sigma ^ 2)) * $t) / ($sigma * sqrt($t));
+    my $a2 = (log($S / $S_min) + ($mu - 0.5 * ($sigma ^ 2)) * $t) / ($sigma * sqrt($t));
+
+    my $L_min_delta;
+
+    if ($mu == 0) {
+        $l_min_delta = exp(-$r_q * $t) * $sigma * sqrt($t) * dnorm($a1);
+        $l_min_delta = $l_min_delta + exp(-$r_q * $t) * (pnorm($a1) - 1) * (log($S / $S_min) + 1 + (0.5 * ($sigma ^ 2) * $t));
+    } else {
+        $l_min_delta = (exp(($mu - $r_q) * $t) * 0.5 * $sigma ^ 2 * pnorm(-$a1)) / $mu;
+        $l_min_delta = $l_min_delta - 1 +
+            0.5 * $sigma ^ 2 * exp(-$r_q * t) * ($S / $S_min) ^ (-2 * $mu / ($sigma ^ 2)) * pnorm(-$a1 + (2 * $mu * sqrt($t)) / $sigma);
+    }
+
+    return $l_min_delta;
+}
+
+sub _l_max_delta {
+    return 0.0;
+}
+
+sub dnorm {    # Standard normal density function
+    my $x  = shift;
+    my $pi = 3.14159265359;
+
+    my $value = exp(-$x**2 / 2) / sqrt(2.0 * $pi);
+
+    return $value;
 }
 
 1;
