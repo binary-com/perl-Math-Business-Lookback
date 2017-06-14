45c55f4d3e16ccd99fc9a26b69c5a959bc53e0ed
Shahrizal Shaari <shahrizal@binary.com>
2017-06-14 01:30:07 +0000

    placeholder for greeks

---
+++ lib/Math/Business/LookBacks/Greeks/Vega.pm
@@ -0,0 +1,28 @@
+package Math::Business::LookBacks::::Greeks::Vega;
+use strict;
+use warnings;
+
+use List::Util qw(max min);
+use Math::CDF qw(pnorm);
+
+sub lbfixedcall {
+    return 0.0;
+}
+
+sub lbfixedput {
+    return 0.0;
+}
+
+sub lbfloatcall {
+    return 0.0;
+}
+
+sub lbfloatput {
+    return 0.0;
+}
+
+sub lbhighlow {
+    return 0.0;
+}
+
+1;
