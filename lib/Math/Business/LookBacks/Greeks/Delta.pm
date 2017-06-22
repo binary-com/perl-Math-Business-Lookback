307a8aa4524f9b026ed2f50d0b70eb310db0540c
Shahrizal Shaari <shahrizal@binary.com>
2017-06-22 08:49:03 +0000

    fixing delta

--- lib/Math/Business/LookBacks/Greeks/Delta.pm
+++ lib/Math/Business/LookBacks/Greeks/Delta.pm
@@ -100,7 +100,7 @@ sub lbhighlow {
 sub _a1 {
     my ($S, $K, $t, $r_q, $mu, $sigma, $S_min_or_max) = @_;
 
-    my $a1 = (log($S / $S_min_or_max) + ($mu + 0.5 * ($sigma ^ 2)) * $t) / ($sigma * sqrt($t));
+    my $a1 = (log($S / $S_min_or_max) + ($mu + (0.5 * ($sigma**2))) * $t) / ($sigma * sqrt($t));
 
     return $a1;
 }
@@ -108,7 +108,7 @@ sub _a1 {
 sub _a2 {
     my ($S, $K, $t, $r_q, $mu, $sigma, $S_min_or_max) = @_;
 
-    my $a2 = (log($S / $S_min_or_max) + ($mu - 0.5 * ($sigma ^ 2)) * $t) / ($sigma * sqrt($t));
+    my $a2 = (log($S / $S_min_or_max) + ($mu - 0.5 * ($sigma**2)) * $t) / ($sigma * sqrt($t));
 
     return $a2;
 }
@@ -122,11 +122,11 @@ sub _l_min_delta {
 
     if ($mu == 0) {
         $l_min_delta = exp(-$r_q * $t) * $sigma * sqrt($t) * dnorm(-$a1);
-        $l_min_delta = $l_min_delta + exp(-$r_q * $t) * (pnorm($a1) - 1) * (log($S / $S_min) + 1 + (0.5 * ($sigma ^ 2) * $t));
+        $l_min_delta = $l_min_delta + exp(-$r_q * $t) * (pnorm($a1) - 1) * (log($S / $S_min) + 1 + (0.5 * ($sigma**2) * $t));
     } else {
-        $l_min_delta = (exp(($mu - $r_q) * $t) * 0.5 * $sigma ^ 2 * pnorm(-$a1)) / $mu;
+        $l_min_delta = (exp(($mu - $r_q) * $t) * 0.5 * $sigma**2 * pnorm(-$a1)) / $mu;
         $l_min_delta = $l_min_delta - 1 +
-            0.5 * $sigma ^ 2 * exp(-$r_q * $t) * ($S / $S_min) ^ (-2 * $mu / ($sigma ^ 2)) * pnorm(-$a1 + (2 * $mu * sqrt($t)) / $sigma);
+            0.5 * ($sigma**2) * exp(-$r_q * $t) * ($S / $S_min)**(-2 * $mu / ($sigma**2)) * pnorm(-$a1 + (2 * $mu * sqrt($t)) / $sigma);
     }
 
     return $l_min_delta;
@@ -141,10 +141,10 @@ sub _l_max_delta {
 
     if ($mu == 0) {
         $l_max_delta = exp(-$r_q * $t) * $sigma * sqrt($t) * dnorm($b1);
-        $l_max_delta = $l_max_delta + exp(-$r_q * $t) * pnorm($b1) * (log($S / $S_max) + 1 + (0.5 * ($sigma ^ 2) * $t));
+        $l_max_delta = $l_max_delta + exp(-$r_q * $t) * pnorm($b1) * (log($S / $S_max) + 1 + (0.5 * ($sigma**2) * $t));
     } else {
         $l_max_delta =
-            1 + 0.5 * $sigma ^ 2 * exp(-$r_q * $t) * ($S / $S_max) ^ (-2 * $mu / ($sigma ^ 2)) * pnorm($b1 - (2 * $mu * sqrt($t)) / $sigma);
+            1 + 0.5 * $sigma**2 * exp(-$r_q * $t) * ($S / $S_max) ^ (-2 * $mu / ($sigma**2)) * pnorm($b1 - (2 * $mu * sqrt($t)) / $sigma);
     }
 
     return $l_max_delta;
